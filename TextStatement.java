6c0919eaaaba8c04b0317ee503ad7709e0dd296c
Vitor A. R. Oliveira <32375749+sussa1@users.noreply.github.com>
2021-08-02 22:25:19 +0000

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,23 +1,29 @@
 import java.util.Enumeration;
 
-
 public class TextStatement extends Statement {
   public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
+      String result = getResultName(aCustomer);
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
          //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
+         result += getFigures(each);
       }
       //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
+      result += getFooterLines(aCustomer);
       return result;
    }
+
+   private String getResultName(Customer aCustomer) {
+    return "Rental Record for " + aCustomer.getName() + "\n";
+  }
+
+  private String getFigures(Rental each) {
+    return "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
+  }
+
+  private String getFooterLines(Customer aCustomer) {
+    return "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n" +
+      "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
+  }
 }
