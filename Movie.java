d94d481254bc07ef084eb4e912c46aa57d8eea6b
Vitor A. R. Oliveira <32375749+sussa1@users.noreply.github.com>
2021-08-02 21:51:50 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,8 +41,6 @@ public class Movie {
   }
 
   public int getFrequentRenterPoints(int daysRented){
-    if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-          daysRented > 1) return 2;
-      return 1;
-   }
+    return _price.getFrequentRenterPoints(daysRented);
+  }
 }
