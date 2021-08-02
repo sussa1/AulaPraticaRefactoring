f8c8caecfdde594623cd3fee562e6482011057af
Vitor A. R. Oliveira <32375749+sussa1@users.noreply.github.com>
2021-08-02 21:53:40 +0000

    Commit 15

--- Price.java
+++ Price.java
@@ -4,9 +4,7 @@ public abstract class Price {
    public abstract double getCharge(int daysRented);
 
   public int getFrequentRenterPoints(int daysRented) {
-    if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
-          daysRented > 1) return 2;
-      return 1;
+       return 1;
    }
    
 }
